3389c35e4b3833e85f6f8e2cc0b03cef747e21ac
null_null <nullnullprogramming@gmail.com>
2019-07-17 21:27:30 +0900

    commit

---
+++ DataBase/ch02/CreateStaff.py
@@ -0,0 +1,3 @@
+import sqlite3
+conn = sqlite3.connect("shop.db", isolation_level=None)
+
---
+++ Python/10~20/use_testmodule.py
@@ -0,0 +1,6 @@
+import testmodule
+
+print(testmodule.a)
+print(testmodule.b)
+testmodule.foo()
+print(testmodule.sys.argv)
