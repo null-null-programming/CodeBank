730358d97bbd6b934a3be095b3c8354e809e6ae5
null_null <nullnullprogramming@gmail.com>
2019-07-18 21:53:49 +0900

    commit

---
+++ JavaScript/1~10/comparison_operator.js
@@ -0,0 +1,15 @@
+console.log(1 == true); //true
+
+var data1 = ['a', 'b', 'c'];
+var data2 = ['a', 'b', 'c'];
+console.log(data1 == data2) //false !
+
+// == != ===
+console.log('3.14E2' == 314); //true
+console.log('3.14E2' === 314); //false
+
+//
+var x = 80;
+console.log((x > 70) ? '合格' : '不合格');
+
+//
