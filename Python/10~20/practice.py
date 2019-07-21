f51b2a5924498763a6c1aacc15beb25f6ae41c52
null_null <nullnullprogramming@gmail.com>
2019-07-21 17:59:38 +0900

    commit

---
+++ Python/10~20/practice.py
@@ -0,0 +1,29 @@
+print("Hello,\nWorld")
+
+print(r"home\mydrive\name")
+
+print('''
+    Hello,
+    World,
+    Hello
+ ''')
+
+print("Hello" * 10)
+
+word = "Hello"
+print(word[1])
+print(word[1:4])
+print(word[1:])
+print(word[:4])
+
+#word[1] = 'o'
+# print(word)
+# Error
+
+print(len(word))
+print("Hello World".replace("Hello", "Hollo"))
+
+num = "123"
+num = int(num)
+num2 = 123
+num2 = str(123)
